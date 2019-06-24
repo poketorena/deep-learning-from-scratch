a5c33c75f37877af1e0fd4602ef2839073f8b594
poketorena <poketorena@users.noreply.github.com>
2019-06-24 20:31:21 +0900

    AND, NAND, ORを使ったXORの実装

--- main.py
+++ main.py
@@ -34,21 +34,34 @@ def OR(x1, x2):
         return 1
 
 
+def XOR(x1, x2):
+    s1 = NAND(x1, x2)
+    s2 = OR(x1, x2)
+    y = AND(s1, s2)
+    return y
+
+
 # main program
 print("ANDゲート")
-print(AND(0, 0))
-print(AND(0, 1))
-print(AND(1, 0))
-print(AND(1, 1))
+print((0, 0), AND(0, 0))
+print((0, 1), AND(0, 1))
+print((1, 0), AND(1, 0))
+print((1, 1), AND(1, 1))
 
 print("NANDゲート")
-print(NAND(0, 0))
-print(NAND(0, 1))
-print(NAND(1, 0))
-print(NAND(1, 1))
+print((0, 0), NAND(0, 0))
+print((0, 1), NAND(0, 1))
+print((1, 0), NAND(1, 0))
+print((1, 1), NAND(1, 1))
 
 print("ORゲート")
-print(OR(0, 0))
-print(OR(0, 1))
-print(OR(1, 0))
-print(OR(1, 1))
+print((0, 0), OR(0, 0))
+print((0, 1), OR(0, 1))
+print((1, 0), OR(1, 0))
+print((1, 1), OR(1, 1))
+
+print("XORゲート")
+print((0, 0), XOR(0, 0))
+print((0, 1), XOR(0, 1))
+print((1, 0), XOR(1, 0))
+print((1, 1), XOR(1, 1))
