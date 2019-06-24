f104f7bfaaaa51b2138846ea98141b250970884f
poketorena <poketorena@users.noreply.github.com>
2019-06-24 20:13:20 +0900

    単純パーセプトロンによるANDゲートの実装

--- main.py
+++ main.py
@@ -1,15 +1,34 @@
-class Man:
-    def __init__(self, name):
-        self.name = name
-        print("Initialized!")
+def AND(x1, x2):
+    """
+    単純パーセプトロンによるANDゲートの実装
+    Args:
+        x1:
+        x2:
 
-    def hello(self):
-        print(f"Hello {self.name}!")
+    Returns:
+        ANDゲートの結果
 
-    def goodbye(self):
-        print(f"Good-bye {self.name}!")
+    """
+
+    # 重み1
+    weight1 = 0.5
+
+    # 重み2
+    weight2 = 0.5
+
+    # 閾値（閾値を超えたら発火）
+    theta = 0.7
+
+    tmp = x1 * weight1 + x2 * weight2
+
+    if tmp <= theta:
+        return 0
+    elif tmp > theta:
+        return 1
 
 
-m = Man("David")
-m.hello()
-m.goodbye()
+# mainプログラム
+print(AND(0, 0))
+print(AND(0, 1))
+print(AND(1, 0))
+print(AND(1, 1))
