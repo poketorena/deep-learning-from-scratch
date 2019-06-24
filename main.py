dc5545f0f1399cf68d311527120dae69ea86070c
poketorena <poketorena@users.noreply.github.com>
2019-06-24 20:16:38 +0900

    単純パーセプトロンによるANDゲートの実装（重みとバイアスを導入した）

--- main.py
+++ main.py
@@ -1,34 +1,8 @@
-def AND(x1, x2):
-    """
-    単純パーセプトロンによるANDゲートの実装
-    Args:
-        x1:
-        x2:
+import numpy as np
 
-    Returns:
-        ANDゲートの結果
-
-    """
-
-    # 重み1
-    weight1 = 0.5
-
-    # 重み2
-    weight2 = 0.5
-
-    # 閾値（閾値を超えたら発火）
-    theta = 0.7
-
-    tmp = x1 * weight1 + x2 * weight2
-
-    if tmp <= theta:
-        return 0
-    elif tmp > theta:
-        return 1
-
-
-# mainプログラム
-print(AND(0, 0))
-print(AND(0, 1))
-print(AND(1, 0))
-print(AND(1, 1))
+x = np.array([0, 1])  # 入力
+w = np.array([0.5, 0.5])  # 重み
+b = -0.7  # バイアス
+print(w * x)
+print(np.sum(w * x))
+print(np.sum(w * x) + b)
