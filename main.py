d1518ba3d799c8a3d87b387a3f0b5a6ff7cb8212
poketorena <poketorena@users.noreply.github.com>
2019-06-25 11:00:20 +0900

    ニューラルネットワークの行列の積

--- main.py
+++ main.py
@@ -1,22 +1,13 @@
 import numpy as np
-import matplotlib.pyplot as plt
 
+x = np.array([1, 2])
+print(x)
+print(x.shape)
 
-def step_function(x):
-    return np.array(x > 0, dtype=np.int)
+w = np.array([[1, 3, 5], [2, 4, 6]])
+print(w)
+print(w.shape)
 
-
-def sigmoid(x):
-    return 1 / (1 + np.exp(-x))
-
-
-def relu(x):
-    return np.maximum(0, x)
-
-
-# main program
-x = np.arange(-5.0, 5.0, 0.1)
-y = relu(x)
-plt.plot(x, y)
-plt.ylim(-1, 6.1)  # y軸の範囲を指定
-plt.show()
+y = np.dot(x, w)
+print(y)
+print(y.shape)
