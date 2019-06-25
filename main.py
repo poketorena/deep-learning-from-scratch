0896f4915a04f0a829fb0ecbc540ee7f8579c1e9
poketorena <poketorena@users.noreply.github.com>
2019-06-25 15:09:01 +0900

    オーバーフロー対策したソフトマックス関数の実装

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import numpy as np
 
 
 def softmax(a):
-    exp_a = np.exp(a)
+    c = np.max(a)
+    exp_a = np.exp(a - c)  # オーバーフロー対策
     sum_exp_a = np.sum(exp_a)
     y = exp_a / sum_exp_a
 
@@ -11,4 +12,6 @@ def softmax(a):
 
 data = np.array([0.3, 2.9, 4.0])
 result = softmax(data)
+
 print(result)
+print(np.sum(result))
