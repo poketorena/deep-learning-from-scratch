41e67c30119f74dfc6c21f4b4db04d597aa92e89
poketorena <poketorena@users.noreply.github.com>
2019-06-25 16:29:04 +0900

    バッチ処理を用いたニューラルネットワークの推論処理（MNIST）

--- main.py
+++ main.py
@@ -55,12 +55,12 @@ print(W1.shape)
 print(W2.shape)
 print(W3.shape)
 
+batch_size = 100  # バッチの数
 accuracy_cnt = 0
-for i in range(len(x)):
-    y = predict(network, x[i])
-    p = np.argmax(y)  # 最も確率の高い要素のインデックスを取得
-
-    if p == t[i]:
-        accuracy_cnt += 1
+for i in range(0, len(x), batch_size):
+    x_batch = x[i:i + batch_size]
+    y_batch = predict(network, x_batch)
+    p = np.argmax(y_batch, axis=1)
+    accuracy_cnt += np.sum(p == t[i:i + batch_size])
 
 print(f"Accuracy:{float(accuracy_cnt) / len(x)}")
