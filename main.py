ea568acd5b1f201c8e41844ab7330d535ae91bb3
poketorena <poketorena@users.noreply.github.com>
2019-06-25 19:47:37 +0900

    数値微分の例

--- main.py
+++ main.py
@@ -12,12 +12,25 @@ def function_1(x):
     return 0.01 * x ** 2 + 0.1 * x
 
 
+def tangent_line(f, x):
+    d = numerical_diff(f, x)
+    print(d)
+    y = f(x) - d * x
+    return lambda t: d * t + y
+
+
 x = np.arange(0.0, 20.0, 0.1)  # 0から20まで、0.1刻みのx配列
 y = function_1(x)
 plt.xlabel("x")
 plt.ylabel("y")
 plt.plot(x, y)
+tf1 = tangent_line(function_1, 5)
+y2 = tf1(x)
+plt.plot(x, y2)
+tf2 = tangent_line(function_1, 12.5)
+y3 = tf2(x)
+plt.plot(x, y3)
 plt.show()
 
 print(numerical_diff(function_1, 5))
-print(numerical_diff(function_1, 10))
+print(numerical_diff(function_1, 12.5))
