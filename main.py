bf1062c7d7f263c8b87ead69c88fe4405719edb7
poketorena <poketorena@users.noreply.github.com>
2019-06-25 19:40:08 +0900

    数値微分（微小な値に10^-4を用い、前方差分を用いる）のバグを修正

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 # 微小な値に10^-4を用い、前方差分を用いる
 def numerical_diff(f, x):
     h = 1e-4
-    return (f(x + h) - f(x - h) / (2 * h))
+    return ((f(x + h) - f(x - h)) / (2 * h))
 
 
-# 丸目誤差で正しく計算できない！
-print(np.float32(1e-50))
+def function_1(x):
+    return 0.01 * x ** 2 + 0.1 * x
+
+
+x = np.arange(0.0, 20.0, 0.1)  # 0から20まで、0.1刻みのx配列
+y = function_1(x)
+plt.xlabel("x")
+plt.ylabel("y")
+plt.plot(x, y)
+plt.show()
+
+print(numerical_diff(function_1, 5))
+print(numerical_diff(function_1, 10))
