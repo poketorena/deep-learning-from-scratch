2ec3313c71bf7db09f2c5ca9e396093c5933bde5
poketorena <poketorena@users.noreply.github.com>
2019-06-25 10:50:38 +0900

    ReLUのグラフ

--- main.py
+++ main.py
@@ -10,9 +10,13 @@ def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
 
+def relu(x):
+    return np.maximum(0, x)
+
+
 # main program
 x = np.arange(-5.0, 5.0, 0.1)
-y = sigmoid(x)
+y = relu(x)
 plt.plot(x, y)
-plt.ylim(-0.1, 1.1)  # y軸の範囲を指定
+plt.ylim(-1, 6.1)  # y軸の範囲を指定
 plt.show()
