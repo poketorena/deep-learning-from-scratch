b96503b2628979c49549ae78c4c94b1c7b514878
poketorena <poketorena@users.noreply.github.com>
2019-06-25 18:15:05 +0900

    2乗和誤差

--- main.py
+++ main.py
@@ -1,66 +1,17 @@
 import numpy as np
-import pickle
-from dataset.mnist import load_mnist
-from PIL import Image
 
 
-def softmax(a):
-    c = np.max(a)
-    exp_a = np.exp(a - c)  # オーバーフロー対策
-    sum_exp_a = np.sum(exp_a)
-    y = exp_a / sum_exp_a
-
-    return y
+def mean_squared_error(y, t):
+    return 0.5 * np.sum((y - t) ** 2)
 
 
-def sigmoid(x):
-    return 1 / (1 + np.exp(-x))
+t = [0, 0, 1, 0, 0, 0, 0, 0, 0, 0]
 
+# 2の確立が最も高い場合
+y = [0.1, 0.05, 0.6, 0.0, 0.05, 0.1, 0.0, 0.1, 0.0, 0.0]
 
-def get_data():
-    (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, flatten=True, one_hot_label=False)
-    return x_test, t_test
+# 7の確立が最も高い場合
+# y = [0.1, 0.05, 0.1, 0.0, 0.05, 0.1, 0.0, 0.6, 0.0, 0.0]
 
-
-def init_network():
-    with open("sample_weight.pkl", "rb")as f:
-        network = pickle.load(f)
-    return network
-
-
-def predict(network, x):
-    W1, W2, W3 = network["W1"], network["W2"], network["W3"]
-    b1, b2, b3, = network["b1"], network["b2"], network["b3"]
-
-    a1 = np.dot(x, W1) + b1
-    z1 = sigmoid(a1)
-    a2 = np.dot(z1, W2) + b2
-    z2 = sigmoid(a2)
-    a3 = np.dot(z2, W3) + b3
-    y = softmax(a3)
-
-    return y
-
-
-# main program
-x, t = get_data()
-network = init_network()
-
-# shapeの確認
-W1, W2, W3 = network["W1"], network["W2"], network["W3"]
-
-print(x.shape)
-print(x[0].shape)
-print(W1.shape)
-print(W2.shape)
-print(W3.shape)
-
-batch_size = 100  # バッチの数
-accuracy_cnt = 0
-for i in range(0, len(x), batch_size):
-    x_batch = x[i:i + batch_size]
-    y_batch = predict(network, x_batch)
-    p = np.argmax(y_batch, axis=1)
-    accuracy_cnt += np.sum(p == t[i:i + batch_size])
-
-print(f"Accuracy:{float(accuracy_cnt) / len(x)}")
+result = mean_squared_error(np.array(y), np.array(t))
+print(result)
