c1a3bd2ba2b1606177dabecbd0a4ab26f44903d1
poketorena <poketorena@users.noreply.github.com>
2019-06-25 10:41:43 +0900

    ステップ関数のグラフ

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
 def step_function(x):
-    if x > 0:
-        return 1
-    else:
-        return 0
+    return np.array(x > 0, dtype=np.int)
+
+
+# main program
+x = np.arange(-5.0, 5.0, 0.1)
+y = step_function(x)
+plt.plot(x, y)
+plt.ylim(-0.1, 1.1)  # y軸の範囲を指定
+plt.show()
