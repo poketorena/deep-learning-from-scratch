a5f93bcdfacc685dde4bc510dfc5bf91ccfc0bab
poketorena <poketorena@users.noreply.github.com>
2019-06-25 11:59:24 +0900

    3層ニューラルネットワークの実装2

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 import numpy as np
 
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+
+# 恒等関数（入力をそのまま返す関数）
+def identity_function(x):
+    return x
+
+
+# 0-1層
+
 X = np.array([1.0, 0.5])
 W1 = np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]])
 B1 = np.array([0.1, 0.2, 0.3])
@@ -11,3 +23,32 @@ print(B1.shape)
 A1 = np.dot(X, W1) + B1
 
 print(A1)
+
+Z1 = sigmoid(A1)
+
+print(A1)
+print(Z1)
+
+# 1-2層
+W2 = np.array([[0.1, 0.4], [0.2, 0.5], [0.3, 0.6]])
+B2 = np.array([0.1, 0.2])
+
+print(Z1.shape)
+print(W2.shape)
+print(B2.shape)
+
+A2 = np.dot(Z1, W2) + B2
+Z2 = sigmoid(A2)
+
+print(A2)
+print(Z2)
+
+# 2-出力層
+W3 = np.array([[0.1, 0.3], [0.2, 0.4]])
+B3 = np.array([0.1, 0.2])
+
+A3 = np.dot(Z2, W3) + B3
+Y = identity_function(A3)  # もしくはY=A3
+
+print("A3", A3)
+print("Y", Y)
