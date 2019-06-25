37d460b61a34a0e3ac187c1285021b7e6848ab3b
poketorena <poketorena@users.noreply.github.com>
2019-06-25 18:50:24 +0900

    交差エントロピー誤差

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import numpy as np
 
 
-def mean_squared_error(y, t):
-    return 0.5 * np.sum((y - t) ** 2)
+def cross_entropy_error(y, t):
+    delta = 1e-7
+    return -np.sum(t * np.log(y + delta))
 
 
 t = [0, 0, 1, 0, 0, 0, 0, 0, 0, 0]
@@ -11,7 +12,7 @@ t = [0, 0, 1, 0, 0, 0, 0, 0, 0, 0]
 y = [0.1, 0.05, 0.6, 0.0, 0.05, 0.1, 0.0, 0.1, 0.0, 0.0]
 
 # 7の確立が最も高い場合
-# y = [0.1, 0.05, 0.1, 0.0, 0.05, 0.1, 0.0, 0.6, 0.0, 0.0]
+y = [0.1, 0.05, 0.1, 0.0, 0.05, 0.1, 0.0, 0.6, 0.0, 0.0]
 
-result = mean_squared_error(np.array(y), np.array(t))
+result = cross_entropy_error(np.array(y), np.array(t))
 print(result)
