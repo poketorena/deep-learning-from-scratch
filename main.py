b132b87ac36bc0d0b7a3ad4f38a6e2699fa56a83
poketorena <poketorena@users.noreply.github.com>
2019-06-25 16:13:01 +0900

    ニューラルネットワークの推論処理（MNIST）

--- main.py
+++ main.py
@@ -1,22 +1,57 @@
 import numpy as np
+import pickle
 from dataset.mnist import load_mnist
 from PIL import Image
 
 
-def img_show(img):
-    pil_img = Image.fromarray(np.uint8(img))
-    pil_img.show()
+def softmax(a):
+    c = np.max(a)
+    exp_a = np.exp(a - c)  # オーバーフロー対策
+    sum_exp_a = np.sum(exp_a)
+    y = exp_a / sum_exp_a
+
+    return y
 
 
-# 最初の呼び出しは数分待ちます...
-(x_train, t_train), (x_test, t_test) = load_mnist(flatten=True, normalize=False)
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
 
-img = x_train[0]
-label = t_train[0]
-print(label)
 
-print(img.shape)
-img = img.reshape(28, 28)  # 形状をもとの画像サイズに変形
-print(img.shape)
+def get_data():
+    (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, flatten=True, one_hot_label=False)
+    return x_test, t_test
 
-img_show(img)
+
+def init_network():
+    with open("sample_weight.pkl", "rb")as f:
+        network = pickle.load(f)
+    return network
+
+
+def predict(network, x):
+    W1, W2, W3 = network["W1"], network["W2"], network["W3"]
+    b1, b2, b3, = network["b1"], network["b2"], network["b3"]
+
+    a1 = np.dot(x, W1) + b1
+    z1 = sigmoid(a1)
+    a2 = np.dot(z1, W2) + b2
+    z2 = sigmoid(a2)
+    a3 = np.dot(z2, W3) + b3
+    y = softmax(a3)
+
+    return y
+
+
+# main program
+x, t = get_data()
+network = init_network()
+
+accuracy_cnt = 0
+for i in range(len(x)):
+    y = predict(network, x[i])
+    p = np.argmax(y)  # 最も確率の高い要素のインデックスを取得
+
+    if p == t[i]:
+        accuracy_cnt += 1
+
+print(f"Accuracy:{float(accuracy_cnt) / len(x)}")
