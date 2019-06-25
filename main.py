f2cbaf24d45f37f3574dd9d76f0fde43d5e44fc4
poketorena <poketorena@users.noreply.github.com>
2019-06-25 14:34:03 +0900

    Pythonによる愚直なソフトマックス関数の実装

--- main.py
+++ main.py
@@ -1,43 +1,11 @@
 import numpy as np
 
+a = np.array([0.3, 2.9, 4.0])
+exp_a = np.exp(a)  # 指数関数
+print("exp_a", exp_a)
 
-def sigmoid(x):
-    return 1 / (1 + np.exp(-x))
+sum_exp_a = np.sum(exp_a)  # 指数関数の和
+print("sum_exp_a", sum_exp_a)
 
-
-# 恒等関数（入力をそのまま返す関数）
-def identity_function(x):
-    return x
-
-
-def init_network():
-    network = {}
-    network["W1"] = np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]])
-    network["b1"] = np.array([0.1, 0.2, 0.3])
-    network["W2"] = np.array([[0.1, 0.4], [0.2, 0.5], [0.3, 0.6]])
-    network["b2"] = np.array([0.1, 0.2])
-    network["W3"] = np.array([[0.1, 0.3], [0.2, 0.4]])
-    network["b3"] = np.array([0.1, 0.2])
-
-    return network
-
-
-def forward(network, x):
-    W1, W2, W3 = network["W1"], network["W2"], network["W3"]
-    b1, b2, b3 = network["b1"], network["b2"], network["b3"]
-
-    a1 = np.dot(x, W1) + b1
-    z1 = sigmoid(a1)
-    a2 = np.dot(z1, W2) + b2
-    z2 = sigmoid(a2)
-    a3 = np.dot(z2, W3) + b3
-    y = identity_function(a3)
-
-    return y
-
-
-# main program
-network = init_network()
-x = np.array([1.0, 0.5])
-y = forward(network, x)
-print(y)
+y = exp_a / sum_exp_a
+print("y", y)
