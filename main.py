2f57a984f253d50690b75a24d3faa11d4cf60161
poketorena <poketorena@users.noreply.github.com>
2019-06-25 19:10:42 +0900

    交差エントロピー誤差（one-hot表現ではなく「2」や「7」などのラベルが与えられた場合）

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 import numpy as np
+from dataset.mnist import load_mnist
 
 
 def cross_entropy_error(y, t):
-    delta = 1e-7
-    return -np.sum(t * np.log(y + delta))
+    if y.ndim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
+
+    batch_size = y.shape[0]
+    return -np.sum(np.log(y[np.arange(batch_size), t] + 1e-7)) / batch_size
 
 
-t = [0, 0, 1, 0, 0, 0, 0, 0, 0, 0]
+(x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
 
-# 2の確立が最も高い場合
-y = [0.1, 0.05, 0.6, 0.0, 0.05, 0.1, 0.0, 0.1, 0.0, 0.0]
+print(x_train.shape)
+print(t_train.shape)
 
-# 7の確立が最も高い場合
-y = [0.1, 0.05, 0.1, 0.0, 0.05, 0.1, 0.0, 0.6, 0.0, 0.0]
-
-result = cross_entropy_error(np.array(y), np.array(t))
-print(result)
+train_size = x_train.shape[0]
+batch_size = 10
+batch_mask = np.random.choice(train_size, batch_size)
+x_batch = x_train[batch_mask]
+t_batch = t_train[batch_mask]
