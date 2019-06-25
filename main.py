f085c8fe2e7bace5580afbf504575960515e994a
poketorena <poketorena@users.noreply.github.com>
2019-06-25 10:44:36 +0900

    シグモイド関数のグラフ

--- main.py
+++ main.py
@@ -6,9 +6,13 @@ def step_function(x):
     return np.array(x > 0, dtype=np.int)
 
 
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+
 # main program
 x = np.arange(-5.0, 5.0, 0.1)
-y = step_function(x)
+y = sigmoid(x)
 plt.plot(x, y)
 plt.ylim(-0.1, 1.1)  # y軸の範囲を指定
 plt.show()
