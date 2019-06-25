8d42b81943a2db9f861e7427118c523055ca910f
poketorena <poketorena@users.noreply.github.com>
2019-06-25 19:58:52 +0900

    数値微分を使って偏微分を求める

--- main.py
+++ main.py
@@ -8,29 +8,15 @@ def numerical_diff(f, x):
     return ((f(x + h) - f(x - h)) / (2 * h))
 
 
-def function_1(x):
-    return 0.01 * x ** 2 + 0.1 * x
+# x0=3、x1=4の時のx0に対する偏微分を求めよ
+def function_tmp1(x0):
+    return x0 * x0 + 4.0 ** 2
 
 
-def tangent_line(f, x):
-    d = numerical_diff(f, x)
-    print(d)
-    y = f(x) - d * x
-    return lambda t: d * t + y
+# x0=3、x1=4の時のx1に対する偏微分を求めよ
+def function_tmp2(x1):
+    return 3.0 ** 2.0 + x1 * x1
 
 
-x = np.arange(0.0, 20.0, 0.1)  # 0から20まで、0.1刻みのx配列
-y = function_1(x)
-plt.xlabel("x")
-plt.ylabel("y")
-plt.plot(x, y)
-tf1 = tangent_line(function_1, 5)
-y2 = tf1(x)
-plt.plot(x, y2)
-tf2 = tangent_line(function_1, 12.5)
-y3 = tf2(x)
-plt.plot(x, y3)
-plt.show()
-
-print(numerical_diff(function_1, 5))
-print(numerical_diff(function_1, 12.5))
+print(numerical_diff(function_tmp1, 3.0))
+print(numerical_diff(function_tmp2, 4.0))
