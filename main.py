d58d3ce73341025b8c7eb2b72db6975d7914ceba
poketorena <poketorena@users.noreply.github.com>
2019-06-25 15:45:31 +0900

    MNIST画像の表示

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 import numpy as np
+from dataset.mnist import load_mnist
+from PIL import Image
 
 
-def softmax(a):
-    c = np.max(a)
-    exp_a = np.exp(a - c)  # オーバーフロー対策
-    sum_exp_a = np.sum(exp_a)
-    y = exp_a / sum_exp_a
-
-    return y
+def img_show(img):
+    pil_img = Image.fromarray(np.uint8(img))
+    pil_img.show()
 
 
-data = np.array([0.3, 2.9, 4.0])
-result = softmax(data)
+# 最初の呼び出しは数分待ちます...
+(x_train, t_train), (x_test, t_test) = load_mnist(flatten=True, normalize=False)
 
-print(result)
-print(np.sum(result))
+img = x_train[0]
+label = t_train[0]
+print(label)
+
+print(img.shape)
+img = img.reshape(28, 28)  # 形状をもとの画像サイズに変形
+print(img.shape)
+
+img_show(img)
