0ee42ae7302a7e1b7c5c787ce89f24725d6e74f4
poketorena <poketorena@users.noreply.github.com>
2019-06-26 19:57:52 +0900

    2層のニューラルネットワークの実装（テストデータで評価するようにした & 完成）

--- train_neuralnet_by_mini_batch.py
+++ train_neuralnet_by_mini_batch.py
@@ -1,3 +1,4 @@
+import sys
 import numpy as np
 import matplotlib.pyplot as plt
 from dataset.mnist import load_mnist
@@ -6,12 +7,17 @@ from two_layer_net import TwoLayerNet
 (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
 
 train_loss_list = []
+train_acc_list = []
+test_acc_list = []
 
 # ハイパーパラメータ
 iters_num = 10000
 train_size = x_train.shape[0]
 batch_size = 100
 learnig_rate = 0.1
+
+# 1エポック当たりの繰り返し数
+iter_per_epoch = max(train_size / batch_size, 1)
 
 network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
 
@@ -33,6 +39,14 @@ for i in range(iters_num):
     loss = network.loss(x_batch, t_batch)
     train_loss_list.append(loss)
 
+    # 1エポックごとに認識精度を計算
+    if i % iter_per_epoch == 0:
+        train_acc = network.accuracy(x_train, t_train)
+        test_acc = network.accuracy(x_test, t_test)
+        train_acc_list.append(train_acc)
+        test_acc_list.append(test_acc)
+        print(f"train acc, test acc | {train_acc}, {test_acc}")
+
 # 損失の可視化
 x = range(1, 10001)
 plt.plot(x, train_loss_list)
@@ -40,3 +54,16 @@ plt.xlabel("iteration")
 plt.ylabel("loss")
 plt.savefig("loss_per_iteration_iter10000.png")
 plt.show()
+
+# グラフの描画
+markers = {'train': 'o', 'test': 's'}
+x = np.arange(len(train_acc_list))
+plt.plot(x, train_acc_list, label='train acc')
+plt.plot(x, test_acc_list, label='test acc', linestyle='--')
+plt.xlabel("epochs")
+plt.ylabel("accuracy")
+plt.ylim(0, 1.0)
+plt.legend(loc='lower right')
+plt.show()
+
+sys.exit()
