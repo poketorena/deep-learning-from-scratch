5186bf09fdb2da8d426cc6994cf34400a59702e7
poketorena <poketorena@users.noreply.github.com>
2019-06-26 16:52:41 +0900

    2層ニューラルネットワークの実装1

---
+++ two_layer_net.py
@@ -0,0 +1,96 @@
+from common.functions import *
+from common.gradient import numerical_gradient
+
+
+class TwoLayerNet:
+
+    def __init__(self, input_size, hidden_size, output_size, weight_init_std=0.01):
+        # 重みの初期化
+        self.params = {}
+        self.params["W1"] = weight_init_std * np.random.randn(input_size, hidden_size)
+
+        self.params["b1"] = np.zeros(hidden_size)
+        self.params["W2"] = weight_init_std * np.random.randn(hidden_size, output_size)
+        self.params["b2"] = np.zeros(output_size)
+
+    def predict(self, x):
+        W1, W2 = self.params["W1"], self.params["W2"]
+        b1, b2 = self.params["b1"], self.params["b2"]
+
+        a1 = np.dot(x, W1) + b1
+        z1 = sigmoid(a1)
+        a2 = np.dot(z1, W2) + b2
+        y = softmax(a2)
+
+        return y
+
+    def loss(self, x, t):
+        """損失を求めるメソッド
+
+        Args:
+            x: 入力データ
+            t: 教師データ
+
+        Returns:
+            損失（クロスエントロピー）
+
+        """
+
+        y = self.predict(x)
+
+        return cross_entropy_error(y, t)
+
+    def accuracy(self, x, t):
+        y = self.predict(x)
+        y = np.argmax(y, axis=1)
+        t = np.argmax(t, axis=1)
+
+        accuracy = np.sum(y == t) / float(x.shape[0])
+        return accuracy
+
+    def numerical_gradient(self, x, t):
+        """勾配を求めるメソッド
+
+        Args:
+            x: 入力データ
+            t: 教師データ
+
+        Returns:
+            勾配
+
+        """
+
+        loss_W = lambda W: self.loss(x, t)
+
+        grads = {}
+
+        grads["W1"] = numerical_gradient(loss_W, self.params["W1"])
+        grads["b1"] = numerical_gradient(loss_W, self.params["b1"])
+        grads["W2"] = numerical_gradient(loss_W, self.params["W2"])
+        grads["b2"] = numerical_gradient(loss_W, self.params["b2"])
+
+        return grads
+
+
+# main program
+net = TwoLayerNet(input_size=784, hidden_size=100, output_size=10)
+print(net.params["W1"].shape)
+print(net.params["b1"].shape)
+print(net.params["W2"].shape)
+print(net.params["b2"].shape)
+
+# 推論処理
+# x = np.random.rand(100, 784)
+# y = net.predict(x)
+# print(y)
+
+x = np.random.rand(100, 784)  # ダミーデータの入力データ
+t = np.random.rand(100, 10)  # ダミーデータの正解ラベル
+
+grads = net.numerical_gradient(x, t)  # 勾配を計算
+
+print(grads["W1"].shape)
+print(grads["b1"].shape)
+print(grads["W2"].shape)
+print(grads["b2"].shape)
+print(grads)
